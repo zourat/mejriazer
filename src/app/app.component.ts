6c1d1c5b454c32a92110738e8929cf61bf4bf460
Azer Mejri <mejriazer@gmail.com>
2024-05-23 17:48:41 +0100

    Version Initiale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, MatButtonModule, MatSlideToggleModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
